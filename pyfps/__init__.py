347b4afb91a4b4ca2e15e17d30bac17e0391ed61
bjtj <bjtj10@gmail.com>
2019-01-03 15:42:52 +0900

    Version: 1.0.1
    * update: pass function arguments

--- pyfps/__init__.py
+++ pyfps/__init__.py
@@ -1,3 +1,8 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
 import time
 
 
@@ -9,16 +14,17 @@ class Fps:
     def __init__(self, func=_print_fps):
         self.func = func
         self.reset()
+        self.second = 1.0
 
     def reset(self):
         self.fps = 0
         self.tick = time.time()
 
-    def update(self):
+    def update(self, *argc, **args):
         dur = time.time() - self.tick
-        if dur >= 1.0:
+        if dur >= self.second:
             if self.func is not None:
-                self.func(self.fps)
+                self.func(self.fps, *argc, **args)
             self.reset()
         else:
             self.fps += 1
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open('README.md', 'r') as fh:
     long_description = fh.read()
 
 setuptools.setup(name='pyfps',
-                 version='1.0.0',
+                 version='1.0.1',
                  author='bjtj',
                  author_email='bjtj10@gmail.com',
                  description='python fps tool',
